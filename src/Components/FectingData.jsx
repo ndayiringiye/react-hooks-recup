185bd7dd69d3dbea684fd468f421b722c0292f23
ndayiringiye david <ndayiringiyedavid120@gmail.com>
2025-02-17 11:14:32 +0200

    chore:on fetching component had been some change

--- src/Components/FectingData.jsx
+++ src/Components/FectingData.jsx
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
 
 const FectingData = () => {
-    const [posts, setPosts] = useState(null);
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
 
     async function intergrateWithUseEffect() {
         try {
+            setIsLoading(true)
             const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-            const data = response.json();
+            const data =await  response.json();
             console.log(data);
             setPosts(data)
             console.log(posts)
         } catch (error) {
             console.log("fetching failed", error.message)
+        }finally{
+            setIsLoading(false);
         }
 
     };
@@ -23,7 +28,10 @@ const FectingData = () => {
     return (
         <div>
             <div>
-                {posts && posts.map((post, index) => (
+               {isLoading ? <p>loading ....</p> : null}
+            </div>
+            <div>
+                {posts.map((post, index) => (
                     <div key={index}>
                         <h1>{post.body}</h1>
                         <p>{post.title}</p>
