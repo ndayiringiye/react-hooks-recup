1a5ed5c923570d906b5cc8c52a24e979d23dfc15
ndayiringiye david <ndayiringiyedavid120@gmail.com>
2025-02-17 11:28:11 +0200

    chore:tailwindcss is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import ContextManagement from "./Components/ContextManagement";
 import Counter from "./Components/Counter";
 import FectingData from "./Components/FectingData";
 const App = () => {
   return (
     <div>
+      <ContextManagement />
      <Counter />
      <FectingData />
     </div>
---
+++ src/Components/ContextManagement.jsx
@@ -0,0 +1,12 @@
+
+const ContextManagement = () => {
+    return (
+        <div>
+            <h1 class="text-3xl font-bold underline text-blue-500">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
+
+export default ContextManagement
